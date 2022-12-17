7f1123a8c5c7763ac22c49c328612505430e25ad
vinayak shukla <65018895+vinayakfaster@users.noreply.github.com>
2022-12-17 17:27:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const sushiswap = require('./sushiswap.json');
+const Uniswap = require('./Uniswap.json');
+const flashloan = require('./flashloan.json')
+const quickswap = require('./quickswap.json')
+const apeswap = require('./apeswap.json')
+
+module.exports = {
+  sushiswap,
+  Uniswap,
+  quickswap,
+  flashloan,
+  apeswap
+};
